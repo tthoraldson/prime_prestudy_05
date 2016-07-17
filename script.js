61d61ad638d0ab677f4b0ef601f0271ab11363e0
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2016-07-17 13:33:50 -0500

    initial file upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Theresa Thoraldson
+// Prime Prework Week 5
+
+var avengersMovie = {
+    name: "Avengers",
+    runtime: 143,
+    releaseYear: 2012,
+    director: "Joss Whedon"
+};
+
+console.log(avengersMovie);
